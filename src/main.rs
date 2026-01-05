7f20501e74650d932e65546c7bb0455e1fbe6565
Konstantin Tutsch <mail@konstantintutsch.com>
2026-01-05 09:14:05 +0100

    feat: concurrency of client and server
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::{io, env};
-use log::error;
+use std::{io, thread};
 
 mod networking;
 use networking::server;
@@ -8,37 +7,27 @@ use networking::client;
 fn main() {
     env_logger::init();
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        error!("Not enough arguments passed. Please choose: server, client");
-        return;
-    }
+    let mut host = String::new();
+    let mut target = String::new();
 
-    if args[1] == "server" {
-        let mut host = String::new();
+    println!("Listen on [host:port]:");
+    let host_n = io::stdin().read_line(&mut host).unwrap();
+    host.truncate(host_n - 1); // Remove trailing \n from input
 
-        println!("Listen on [host:port]:");
-        let host_n = io::stdin().read_line(&mut host).unwrap();
-        host.truncate(host_n - 1); // Remove trailing \n from input
-
+    thread::spawn(|| {
         server::listen(host);
-    } else if args[1] == "client" {
-        let mut target = String::new();
+    });
 
-        println!("Send to [host:port]:");
-        let target_n = io::stdin().read_line(&mut target).unwrap();
-        target.truncate(target_n - 1); // Remove trailing \n from input
+    println!("Send to [host:port]:");
+    let target_n = io::stdin().read_line(&mut target).unwrap();
+    target.truncate(target_n - 1); // Remove trailing \n from input
 
-        loop {
-            let mut message = String::new();
+    loop {
+        let mut message = String::new();
 
-            println!("Enter message: ");
-            let message_n = io::stdin().read_line(&mut message).unwrap();
-            message.truncate(message_n - 1); // Remove trailing \n from input
+        let message_n = io::stdin().read_line(&mut message).unwrap();
+        message.truncate(message_n - 1); // Remove trailing \n from input
 
-            client::send(&target, &message);
-        }
-    } else {
-        error!("Unknown option: {}", args[1]);
+        client::send(&target, &message);
     }
 }
